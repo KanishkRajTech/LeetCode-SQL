e80dbcfcabee5a80f3b61ced308a82460bdc0bc6
Kanishk Raj <kanishkraj601@gmail.com>
2026-08-27 11:35:01 +0530

    Customer Who Visited but Did Not Make Any Transactions

---
+++ Customer Who Visited but Did Not Make Any Transactions/Customer Who Visited but Did Not Make Any Transactions.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT V.customer_id, COUNT(v.visit_id) AS count_no_trans
+FROM VISITS V
+LEFT JOIN  Transactions T
+ON T.visit_id  = V.visit_id 
+WHERE T.transaction_id IS NULL
+GROUP BY V.customer_id;
