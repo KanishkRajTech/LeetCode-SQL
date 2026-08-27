43ac32404142cd4fbe94a367b0718d8de845a159
Kanishk Raj <kanishkraj601@gmail.com>
2026-08-27 12:22:27 +0530

    Average Time of Process per Machine

--- Average Time of Process per Machine/Average Time of Process per Machine.sql
+++ Average Time of Process per Machine/Average Time of Process per Machine.sql
@@ -4,5 +4,7 @@ FROM Activity A1
 JOIN Activity A2
 ON A1.machine_id = A2.machine_id 
 AND A1.process_id  = A2.process_id 
-AND A1.timestamp  < A2.timestamp
+-- AND A1.timestamp  < A2.timestamp
+AND A1.activity_type = 'start' 
+AND A2.activity_type = 'end'
 GROUP BY A1.machine_id;
