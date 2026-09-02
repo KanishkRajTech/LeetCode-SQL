5bf6caf3ea38ac42b6edd58f8abcc02eb944e491
Kanishk Raj <kanishkraj601@gmail.com>
2026-09-02 21:06:44 +0530

    Project Employees I

--- Project Employees I/Project Employees I.sql
+++ Project Employees I/Project Employees I.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 SELECT p.project_id,ROUND( AVG(E.experience_years ),2 )AS average_years
-FROM Employee E
-LEFT JOIN Project P
+FROM Project P 
+LEFT JOIN  Employee E
 ON P.employee_id = E.employee_id 
 GROUP BY p.project_id
